83c813f1232c22602897d031494b7abeadd52197
Timothy Gilmour <tim.g.gilmour@gmail.com>
2020-03-12 16:34:47 -0500

    Added Titles and Basic Tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Giving Gardens"
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get gardens" do
-    get static_pages_gardens_url
+    get gardens_url
     assert_response :success
+    assert_select "title", "Gardens | #{@base_title}"
   end
 
   test "should get getinvolved" do
-    get static_pages_getinvolved_url
+    get getinvolved_url
     assert_response :success
+    assert_select "title", "Get Involved | #{@base_title}"
   end
 
 end
