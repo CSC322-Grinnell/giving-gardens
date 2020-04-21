50e110740e12115012c739985ef5b05f4eb0e53b
Thomas Pinkava <pinkavaprime@gmail.com>
2020-04-21 12:46:12 -0700

    wrote Users CRUD controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+    def index
+        render 'temp'
+    end
+
+
+    def new
+        render 'temp'
+    end
+
+
+    def create
+        render 'temp'
+    end
+
+
+    def show
+        render 'temp'
+    end
+
+
+    def edit
+        render 'temp'
+    end
+
+
+    def update
+        render 'temp'
+    end
+
+
+    def destroy
+        render 'temp'
+    end
+end
