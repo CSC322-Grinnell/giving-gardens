3c7e78f44e61d444e3def3a87cf3a567d8d5ed5c
Thomas Pinkava <pinkavaprime@gmail.com>
2020-04-22 13:04:30 -0700

    added old migrations

--- db/migrate/20190304214942_create_garden_produces.rb
+++ db/migrate/20190304214942_create_garden_produces.rb
@@ -1,6 +1,11 @@
 class CreateGardenProduces < ActiveRecord::Migration[5.1]
   def change
     create_table :garden_produces do |t|
+      t.string :garden_id
+      t.string :produce_id
+      t.string :available_at
+      t.integer :readiness
+      t.string :planted_at
 
       t.timestamps
     end
--- db/migrate/20191027145733_create_crops.rb
+++ db/migrate/20191027145733_create_crops.rb
@@ -1,6 +1,10 @@
 class CreateCrops < ActiveRecord::Migration[5.1]
   def change
     create_table :crops do |t|
+      t.integer :produce
+      t.integer :garden_id
+      t.string :planted_at
+      t.text :description
 
       t.timestamps
     end
